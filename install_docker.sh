70e2663faf44ab7fda076fa8d5b4e276f40e1f5b
Jack3113 <jack03113@gmail.com>
2018-01-08 14:53:58 +0000

    feat(docker): Install docker-compose

--- install_docker.sh
+++ install_docker.sh
@@ -15,3 +15,6 @@ sudo add-apt-repository \
 sudo apt-get update
 sudo apt-get install docker-ce --yes
 sudo docker run hello-world
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
