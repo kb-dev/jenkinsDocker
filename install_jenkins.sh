4b827533831d69a5b87d2daddc2e1977e253e99d
Hackjack <jack03113@gmail.com>
2020-01-18 17:30:44 +0100

    Grant sudo for jenkins

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -10,6 +10,7 @@ wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins --yes
+sudo echo 'jenkins ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 
 ## Cypress tools
 sudo apt-get update && apt-get -y install xvfb libgtk2.0-0 libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 uuid-runtime
